f8af500dd94db2e2c06467d951b81110f20e0ca7
Anamika1-cpu <anamikagour666@gmailcom>
2022-12-18 18:38:48 +0530

    category API created

--- routes/category/categoryRoute.js
+++ routes/category/categoryRoute.js
@@ -1,7 +1,21 @@
 const express = require("express");
-const { createCategory } = require("../../controllers/category/Category");
+const {
+  createCategory,
+  fetchAllCategories,
+  updateCategory,
+  deleteCategory,
+} = require("../../controllers/category/Category");
+const authMiddleware = require("../../middlewares/auth/authMiddleware");
 const router = express.Router();
 
-router.post("/", createCategory);
+router.post("/", authMiddleware, createCategory);
+
+router.get("/", authMiddleware, fetchAllCategories);
+
+router.get("/:id", authMiddleware, fetchAllCategories);
+
+router.put("/:id", authMiddleware, updateCategory);
+
+router.delete("/:id", authMiddleware, deleteCategory);
 
 module.exports = router;
